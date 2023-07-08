94db00c3f5fae9a740adb0a5c09e70be586e44e8
shadabtanjeed <stanjeed@gmail.com>
2023-07-08 17:21:11 +0600

    Inifinite Scroll Implementation

--- [Updated]BUETGameJam2023/Assets/Scripts/ParallaxEffect.cs
+++ [Updated]BUETGameJam2023/Assets/Scripts/ParallaxEffect.cs
@@ -6,23 +6,30 @@ public class ParallaxEffect : MonoBehaviour
 {
 
     private float startPos;
+    private float Lenght;
     private GameObject cam;
-    [SerializeField]private float parallaxEffect;
+    [SerializeField] private float parallaxEffect;
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         cam = GameObject.Find("Virtual Camera");
         startPos = transform.position.x;
-        
+        Lenght = gameObject.GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        float temporary = (cam.transform.position.x * (1 - parallaxEffect));
         float distance = (cam.transform.position.x * parallaxEffect);
         transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
-        
+
+        if (temporary > startPos + Lenght)
+        {
+            startPos += Lenght;
+        }
+        else if (temporary < startPos - Lenght)
+        {
+            startPos -= Lenght;
+        }
     }
 }
