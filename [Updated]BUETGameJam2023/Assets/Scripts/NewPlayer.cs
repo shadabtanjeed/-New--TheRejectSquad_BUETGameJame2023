f13a94f1533dbca4b0057713515d8ae2107517c1
shadabtanjeed <stanjeed@gmail.com>
2023-07-09 02:05:47 +0600

    Speed Increses with time

--- [Updated]BUETGameJam2023/Assets/Scripts/NewPlayer.cs
+++ [Updated]BUETGameJam2023/Assets/Scripts/NewPlayer.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NewPlayer : PhysicsObject
-
 {
     [SerializeField] private float maxSpeed = 1;
     [SerializeField] private float jumpPower = 10;
-
+    private float speedIncreaseInterval = 8f;  // Interval for speed increase
+    private float speedIncreaseAmount = 1f;  // Amount to increase speed by
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Start the coroutine for increasing maxSpeed
+        StartCoroutine(IncreaseMaxSpeedRoutine());
+    }
+
+    // Coroutine for increasing maxSpeed
+    IEnumerator IncreaseMaxSpeedRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(speedIncreaseInterval);
+            maxSpeed += speedIncreaseAmount;
+        }
     }
 
     // Update is called once per frame
@@ -20,11 +31,10 @@ public class NewPlayer : PhysicsObject
     {
         targetVelocity = new Vector2(Input.GetAxis("Horizontal") * maxSpeed, 0);
 
-        //If the player presses "Jump" and we're grounded, set the velocity to a jump power value
+        // If the player presses "Jump" and we're grounded, set the velocity to a jump power value
         if (Input.GetButtonDown("Jump") && grounded)
         {
             velocity.y = jumpPower;
         }
     }
-
 }
